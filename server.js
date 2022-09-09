81fbcdc1c749b413fabc76d4a1d4a7276f739bdf
Yuvaraj787 <104787096+Yuvaraj787@users.noreply.github.com>
2022-09-09 20:07:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 app.use(cors({
-    origin:'https://college-review-site.vercel.app/'
+    origin:'*'
 }));
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
